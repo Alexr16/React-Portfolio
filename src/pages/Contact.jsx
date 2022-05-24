a693a415dd20beb5fbf2af1843af1270e157d453
Alexr16 <j.alex-1996@hotmail.com>
2022-05-24 15:13:24 -0500

    Create contact section

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './HomePage.scss';
+
+const Contact = () => (
+  <>
+    <div id="Contact" className="ouline-left">
+      <h3>2024jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+    </div>
+  </>
+);
+export default Contact;
