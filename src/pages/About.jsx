bc21b7178f5b1538194141b977e7510aaae7f38a
Alexr16 <j.alex-1996@hotmail.com>
2022-05-24 17:50:35 -0500

    Add content to the about me section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import './HomePage.scss';
+import me from '../images/me-1.svg';
 
 const About = () => (
   <>
-    <div id="About" className="ouline-left">
-      <h3>2023jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
-      <h3>2022jghjghj</h3>
+    <div id="About" className="about-container">
+      <div className="about-description">
+        <div className="about-title">
+          <h2>About me</h2>
+        </div>
+        <div className="about-paragraph">
+          <p>
+            I can help you build a product, feature or
+            website. Look through some of my work
+            and experience! If you like what you see
+            and have a project you need coded,
+            don&rsquo;t
+            hestiate to contact me.
+          </p>
+        </div>
+        <div>
+          <button type="button" className="about-button">Get My Resume</button>
+        </div>
+      </div>
+      <div className="about-image">
+        <img src={me} alt="profile" width="90%" />
+      </div>
     </div>
   </>
 );
