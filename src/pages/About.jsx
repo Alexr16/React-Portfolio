bbbd4aed9f9fa9fbb4ff9769cddf59a4b88146a5
Alexr16 <j.alex-1996@hotmail.com>
2022-05-24 15:13:15 -0500

    Create about section

---
+++ src/pages/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './HomePage.scss';
+
+const About = () => (
+  <>
+    <div id="About" className="ouline-left">
+      <h3>2023jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+      <h3>2022jghjghj</h3>
+    </div>
+  </>
+);
+export default About;
