a34bac6c6ca87a3f114c752dea877221439f2050
Alexr16 <j.alex-1996@hotmail.com>
2022-05-25 12:57:09 -0500

    Add skills to a about me section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -27,6 +27,51 @@ const About = () => (
         <img src={me} alt="profile" width="90%" />
       </div>
     </div>
+    <hr className="line-space" />
+    <div>
+      <div className="skills-container">
+        <div className="language">
+          <h4>Language</h4>
+          <ul className="list">
+            <li>Javascript</li>
+            <li>Ruby</li>
+            <li>Html</li>
+            <li>Css</li>
+          </ul>
+        </div>
+        <div className="framework">
+          <h4>Frameworks</h4>
+          <ul className="listt">
+            <li>Bootstrap</li>
+            <li>Ruby on Rails</li>
+            <li>RSPec</li>
+            <li>CapyBara</li>
+            <li>Selenium</li>
+          </ul>
+        </div>
+      </div>
+      <div className="skills">
+        <h4>Skills</h4>
+        <div className="skill-list">
+          <span className="chart__label">
+            Codekit
+          </span>
+          <div className="chart__bar" style={{ width: '80%' }} />
+          <span className="chart__label">
+            Github
+          </span>
+          <div className="chart__bar" style={{ width: '95%' }} />
+          <span className="chart__label">
+            Gitlab
+          </span>
+          <div className="chart__bar" style={{ width: '70%' }} />
+          <span className="chart__label">
+            Terminal
+          </span>
+          <div className="chart__bar" style={{ width: '90%' }} />
+        </div>
+      </div>
+    </div>
   </>
 );
 export default About;
