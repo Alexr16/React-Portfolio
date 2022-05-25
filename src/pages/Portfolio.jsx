10d7f08928ed209653c7140cc9574452a5f62e38
Alexr16 <j.alex-1996@hotmail.com>
2022-05-25 17:38:47 -0500

    Style projects section

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,51 +1,42 @@
-import { useSelector } from 'react-redux';
-import React from 'react';
+import React, { useState } from 'react';
 import './HomePage.scss';
+import { useDispatch } from 'react-redux';
+import Technologies from '../components/Badgets';
+import { fetchProjectsData } from '../Redux/Projects/Projects';
+import projectsData from '../Data/projectsData';
+import project from '../images/project1.svg';
 
 const Portfolio = () => {
-  const data = useSelector((state) => state.Projects);
-  console.log(data);
-  //   const visibleData = data.filter((country) => country.confirm > 0);
-  //   const [search, setSearch] = useState(visibleData);
-
-  //   useEffect(() => {
-  //     setSearch(visibleData);
-  //   }, [data]);
-
-  //   const filterData = (e) => {
-  //     const string = e.target.value.toLowerCase();
-  //     const info = data.filter((country) => country.id.includes(string));
-  //     if (info !== 0) {
-  //       setSearch(info);
-  //     } else {
-  //       setSearch(visibleData);
-  //     }
-  //   };
+  const dispatch = useDispatch();
+  const [info] = useState(dispatch(fetchProjectsData(projectsData)));
 
   return (
     <>
-      <div id="Portfolio" className="ouline-left">
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
-        <h3>2022jghjghj</h3>
+      <div id="Portfolio" className="feature-container">
+        <h2>My Recent Works</h2>
+        <div className="project-content">
+          <div className="project-img">
+            <img src={project} alt="profile" width="100%" />
+          </div>
+          <div className="content-container">
+            <p>Feature Project</p>
+            <h3>{info.data[0].title}</h3>
+            <div className="feature-paragraph">
+              <p>{info.data[0].description}</p>
+            </div>
+            <ul className="tech-list">
+              {info.data[0].technologies.map((tec) => (
+                <Technologies
+                  key={tec.id}
+                  item={tec}
+                />
+              ))}
+            </ul>
+            <div className="see-project">
+              <button className="button project-button" type="button">See Project</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
