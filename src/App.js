9626a2f3de327e44320f35f4850a92bab3b5d17c
Alexr16 <j.alex-1996@hotmail.com>
2022-05-25 17:37:12 -0500

    Improve dispatch data method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchProjectsData } from './Redux/Projects/Projects';
-import projectsData from './Data/projectsData';
 import Header from './pages/Header';
 import Portfolio from './pages/Portfolio';
 import NavBar from './components/navBar';
@@ -10,34 +6,27 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Social from './components/Social';
 
-const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchProjectsData(projectsData));
-  }, []);
-
-  return (
-    <Router>
-      <NavBar />
-      <Social />
-      <Routes>
-        <Route
-          path="/"
-          element={(
-            <>
-              <Header />
-              <Portfolio />
-              <About />
-              <Contact />
-            </>
+const App = () => (
+  <Router>
+    <NavBar />
+    <Social />
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <>
+            <Header />
+            <Portfolio />
+            <About />
+            <Contact />
+          </>
 )}
-        />
-        {/* <Route path="/works" element={t} />
+      />
+      {/* <Route path="/works" element={t} />
       <Route path="/about" element={t} />
       <Route path="/contact-form" element={e} /> */}
-      </Routes>
-    </Router>
-  );
-};
+    </Routes>
+  </Router>
+);
 
 export default App;
