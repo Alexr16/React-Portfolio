89708d5fb798caad0f6c5ddbf05dfd96d9dc2efb
Alexr16 <j.alex-1996@hotmail.com>
2022-05-26 17:19:36 -0500

    Add togglepopup action

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import Technologies from './Badgets';
+import PopUP from './PopUp';
 
 const ProjectCard = (prop) => {
   const { project } = prop;
   const [isHovering, setIsHovering] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleMouseEnter = () => {
-    setIsHovering(true);
+    setIsHovering(!isHovering);
   };
 
   const handleMouseLeave = () => {
     setIsHovering(false);
+  };
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
   };
 
   const hover = {
@@ -18,35 +24,46 @@ const ProjectCard = (prop) => {
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
     backgroundSize: 'contain',
-    backgroundColor: isHovering ? '#A5BE3280' : '',
+    backgroundColor: isHovering ? 'rgb(33, 37, 41)' : '',
     color: isHovering ? 'black' : '',
   };
 
   return (
-    <li className="project-container">
-      <div
-        className="project-cont"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        style={hover}
-      >
-        <h3>{project.title}</h3>
-        <div className="project-paragraph">
-          <p>{project.description}</p>
+    <>
+      <li className="project-container">
+        <div
+          className="project-cont"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          style={hover}
+        >
+          <h3>{project.title}</h3>
+          <div className="project-paragraph">
+            <p>{project.description}</p>
+          </div>
+          <ul className="tech-list">
+            {project.technologies.map((tec) => (
+              <Technologies
+                key={tec.id}
+                item={tec}
+              />
+            ))}
+          </ul>
+          <div className="see-btn">
+            <button className="card-project-button" type="button" onClick={togglePopup}>See Project</button>
+          </div>
         </div>
-        <ul className="tech-list">
-          {project.technologies.map((tec) => (
-            <Technologies
-              key={tec.id}
-              item={tec}
-            />
-          ))}
-        </ul>
-        <div className="see-btn">
-          <button className="card-project-button" type="button">See Project</button>
-        </div>
-      </div>
-    </li>
+      </li>
+      <>
+        {isOpen && (
+        <PopUP
+          data={project}
+          handleClose={togglePopup}
+        />
+        )}
+      </>
+
+    </>
   );
 };
 
