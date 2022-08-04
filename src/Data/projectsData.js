52c670539c0cfee84ce6a5985f9314f757e3b9e2
Alexr16 <j.alex-1996@hotmail.com>
2022-08-04 16:56:13 -0500

    Update protfolio

--- src/Data/projectsData.js
+++ src/Data/projectsData.js
@@ -1,9 +1,14 @@
-import image1 from '../images/project1.svg';
+import conference from '../images/conference.png';
+import pokemon from '../images/pokemon.jpg';
+import bookstore from '../images/bookstore.png';
+import covidapp from '../images/covidapp.png';
+import recipeapp from '../images/recipeapp.png';
+import budgetapp from '../images/budgetapp.jpg';
 
 const projectData = [
   {
     id: 1,
-    title: 'Keeping track of hundreds of components website',
+    title: 'Conference Page',
     technologies: [
       {
         id: 't1',
@@ -15,21 +20,21 @@ const projectData = [
       },
       {
         id: 't3',
-        technologie: 'Bootstrap',
+        technologie: 'JavaScript',
       },
     ],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley.',
-    liveButton: 'https://',
-    sourceButton: 'https://',
-    image: image1,
+    description: 'In this project, I build a web page with desktop and mobile versions for a conference using HTML, CSS, and JavaScript.',
+    liveButton: 'https://alexr16.github.io/Conference-page/',
+    sourceButton: 'https://github.com/Alexr16/Conference-page',
+    image: conference,
   },
   {
     id: 2,
-    title: 'Project 1',
+    title: 'Pokemon App',
     technologies: [
       {
         id: 't4',
-        technologie: 'css',
+        technologie: 'SASS',
       },
       {
         id: 't5',
@@ -37,113 +42,109 @@ const projectData = [
       },
       {
         id: 't6',
-        technologie: 'Bootstrap',
+        technologie: 'JavaScript',
       },
       {
         id: 't7',
-        technologie: 'Ruby',
+        technologie: 'Webpack',
       },
     ],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
-    liveButton: 'https://',
-    sourceButton: 'https://',
-    image: image1,
+    description: 'Mobile and desktop version APP showing information about pokemon monsters fetched from PokeAPI.',
+    liveButton: 'https://alexr16.github.io/Pokemon-App/',
+    sourceButton: 'https://github.com/Alexr16/Pokemon-App',
+    image: pokemon,
   },
   {
     id: 3,
-    title: 'Project 2',
+    title: 'Book Store',
     technologies: [
       {
         id: 't8',
-        technologie: 'css',
+        technologie: 'React',
       },
       {
         id: 't9',
-        technologie: 'html',
+        technologie: 'SASS',
       },
       {
         id: 't10',
-        technologie: 'Bootstrap',
+        technologie: 'Redux',
       },
     ],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
-    liveButton: 'https://',
-    sourceButton: 'https://',
-    image: image1,
+    description: 'In this project I build a "Awesome Books" App that allows users to add a book, remove selected book, and display a list of books.',
+    liveButton: 'https://isnt-alexr16-awesome.netlify.app/',
+    sourceButton: 'https://github.com/Alexr16/BookStore',
+    image: bookstore,
   },
   {
     id: 4,
-    title: 'Project 3',
+    title: 'Metrics WebApp',
     technologies: [
       {
         id: 't11',
-        technologie: 'css',
+        technologie: 'React',
       },
       {
         id: 't12',
-        technologie: 'html',
+        technologie: 'SASS',
       },
       {
         id: 't13',
-        technologie: 'Bootstrap',
+        technologie: 'Redux',
       },
       {
         id: 't3',
-        technologie: 'Ruby',
+        technologie: 'JavaScript',
       },
     ],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
-    liveButton: 'https://',
-    sourceButton: 'https://',
-    image: image1,
+    description: 'In this project I built a mobile web application to check a list of countries COVID status making use of React and Redux.',
+    liveButton: 'https://deploy-preview-3--super-cool-site-by-alexr16.netlify.app/',
+    sourceButton: 'https://github.com/Alexr16/Metrics-webapp',
+    image: covidapp,
   },
   {
     id: 5,
-    title: 'Project 4',
+    title: 'Recipe App',
     technologies: [
       {
         id: 't14',
-        technologie: 'css',
+        technologie: 'Ruby',
       },
       {
         id: 't15',
-        technologie: 'html',
+        technologie: 'Ruby on Rails',
       },
       {
         id: 't16',
-        technologie: 'Bootstrap',
+        technologie: 'SASS',
       },
     ],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
-    liveButton: 'https://',
-    sourceButton: 'https://',
-    image: image1,
+    description: 'The Recipe app keeps track of all your recipes, ingredients, and inventory. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app should allow you to make them public so anyone can access them.',
+    liveButton: 'https://calm-mountain-98535.herokuapp.com/',
+    sourceButton: 'https://github.com/Retky/RecipeApp',
+    image: recipeapp,
   },
   {
     id: 6,
-    title: 'Project 5',
+    title: 'Budget App',
     technologies: [
       {
         id: 't17',
-        technologie: 'css',
+        technologie: 'Ruby',
       },
       {
         id: 't18',
-        technologie: 'html',
+        technologie: 'Ruby on Rails',
       },
       {
         id: 't19',
-        technologie: 'Bootstrap',
-      },
-      {
-        id: 't20',
-        technologie: 'Ruby',
+        technologie: 'SASS',
       },
     ],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
-    liveButton: 'https://',
-    sourceButton: 'https://',
-    image: image1,
+    description: 'Mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
+    liveButton: 'https://glacial-waters-55756.herokuapp.com',
+    sourceButton: 'https://github.com/Alexr16/Budget-app',
+    image: budgetapp,
   },
 ];
 
